ade0c7ae662bf9752bb64f147f5776ed2a92d8a2
shilpa <36531617+shilpathota@users.noreply.github.com>
2022-07-05 14:23:35 +0530

    Add files via upload

---
+++ JobEnrichment_CICD_Java/src/main/java/com/daimler/jobenrichment/App.java
@@ -0,0 +1,13 @@
+package com.daimler.jobenrichment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello GSPians! How are you?" );
+    }
+}
